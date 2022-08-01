ac180b37b767cce8be8f6a7aa4ceacfb17399ca7
Bastien <94622009+Fondit@users.noreply.github.com>
2022-08-01 18:07:13 +0200

    C06

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -22,8 +22,44 @@ int	ft_strcmp(char *s1, char *s2)
 	return (s1[i] - s2[i]);
 }
 
+int	ft_print_params(int argc, char **argv)
+{
+	int	i;
+	int	j;
+	
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			write(1, &argv[i][j], 1);
+			j++;
+		}
+		argv[i][j] = '\0';
+		write(1, "\n";
+		i++;
+}
+
 int	main(int argc, char *argv[])
 {
+	char	*temp;
+	int	s;
 	
-	return (0);
+	(void)argc;
+	(void)argv;
+	s = 1;
+	while (s < argc - 1)
+	{
+		if (ft_strcmp(argv[s], argv[s + 1])
+		    {
+			    temp = argv[s];
+			    argv[s] = argv[s + 1];
+			    argv[s + 1] = temp;
+			    s = 1;
+		    }
+		else
+		    s++;
 }
+ft_print_params(argc, argv);
+return (0);
