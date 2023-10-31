8958c830d3fa20d0de7597f3e38db50132fe67bb
Bastien Verdier-Vaissiere <basverdi@2A7.42angouleme.fr>
2023-10-31 19:07:24 +0100

    test

--- Tronc commun/ft_libft/ft_strlcat.c
+++ Tronc commun/ft_libft/ft_strlcat.c
@@ -5,38 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: basverdi <basverdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/31 00:52:00 by basverdi          #+#    #+#             */
-/*   Updated: 2023/10/31 02:02:06 by basverdi         ###   ########.fr       */
+/*   Created: 2023/10/31 18:47:32 by basverdi          #+#    #+#             */
+/*   Updated: 2023/10/31 19:04:48 by basverdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-size_t  ft_strlcat(char *dst, const char *src, size_t size)
-{
-    size_t i;
-    size_t j;
-    size_t  k;
-
-    i = 0;
-    j = ft_strlen(dst);
-    k = ft_strlen(src);
-    if (size == 0 || j == size)
-        return (k + j);
-    if (size == 1)
-    {
-        dst[i] = 0;
-        return (k + j);
-    }
-    while (src[i] && i < size - 1)
-    {
-        dst[i + j] = src[i];
-        i++;
-        
-    }
-    dst[i + j] = '\0';
-    return (ft_strlen(src) + ft_strlen(dst));
-}
 
 size_t  ft_strlcat(char *dst, const char *src, size_t size)
 {
@@ -47,10 +21,10 @@ size_t  ft_strlcat(char *dst, const char *src, size_t size)
 
     i = 0;
     j = 0;
-    dst_len = strlen(dst);
-    src_len = strlen(src);
-    if (size <= dst_len)
-        return (src_len + size);
+    dst_len = ft_strlen(dst);
+    src_len = ft_strlen(src);
+    if (dst == NULL || size == 0)
+        return (0);
     while (dst[i])
         i++;
     while (src[j] && i < size - 1)
@@ -60,32 +34,24 @@ size_t  ft_strlcat(char *dst, const char *src, size_t size)
         j++;
     }
     dst[i] = '\0';
-    return (dst_len + src_len);   
+    if (size <= dst_len)
+        return (src_len + size);
+    return (dst_len + src_len);  
 }
 
-# include <stdio.h>
-# include <string.h>
-# include <stddef.h>
+#include <stdio.h>
+// do a main to test for null and size 0
 
-int main() {
-    char buffer1[30] = "Hello";
-    char buffer2[30] = "Goodbye";
+int main()
+{
+    char    *src;
+    char    *dst;
+    size_t  size;
 
-    printf("Before strlcat:\n");
-    printf("Buffer1: %s\n", buffer1);
-    printf("Buffer2: %s\n", buffer2);
-
-    strlcat(buffer1, ", World!", sizeof(buffer1));
-    strlcat(buffer2, ", Universe!", sizeof(buffer2));
-
-    printf("\nAfter strlcat:\n");
-    printf("Buffer1: %s\n", buffer1);  // Outputs: Hello, World!
-    printf("Buffer2: %s\n", buffer2);  // Outputs: Goodbye, Universe!
-
-    // Edge case: destination buffer size is smaller than the initial content
-    char buffer3[5] = "Hello";
-    strlcat(buffer3, ", World!", sizeof(buffer3));
-    printf("\nBuffer3 (edge case): %s\n", buffer3);  // Outputs: Hell
-
-    return 0;
+    src = "hello";
+    dst = "";
+    size = 0;
+    printf("%zu\n", ft_strlcat((void *)0, src, size));
+    printf("%s\n", dst);
+    return (0);
 }
