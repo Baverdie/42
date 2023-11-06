be3f8b7e15865150ff2ba4debf153f5c052ec9da
Bastien Verdier-Vaissiere <basverdi@3E4.42angouleme.fr>
2023-11-06 10:38:29 +0100

    merge

--- Tronc commun/ft_libft/ft_memchr.c
+++ Tronc commun/ft_libft/ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bastienverdier-vaissiere <bastienverdie    +#+  +:+       +#+        */
+/*   By: basverdi <basverdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 23:45:51 by bastienverd       #+#    #+#             */
-/*   Updated: 2023/11/01 23:48:22 by bastienverd      ###   ########.fr       */
+/*   Updated: 2023/11/06 10:34:15 by basverdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,15 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	char	*src;
 
 	i = 0;
-	if (!s)
+	if (!s || n == 0)
 		return (0);
 	src = (char *)s;
-	if (c == '\0')
+	if (c == 0)
 		return ((char *)&s[ft_strlen(s)]);
-	while (src[i] && i < n)
+	while (i < n)
 	{
-		if (src[i] == c)
-		{
+		if (src[i] == (unsigned char)c)
 			return ((char *)&s[i]);
-		}
 		i++;
 	}
 	return (0);
