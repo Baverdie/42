e3f8853a08b9b4e23e76b8caa8ec612bf7677edd
Fondit <bastien.verdiervaissiere@gmail.com>
2023-11-22 12:11:11 +0100

    printf

--- Tronc commun/Round 2/ft_printf/ft_printf.c
+++ Tronc commun/Round 2/ft_printf/ft_printf.c
@@ -3,55 +3,93 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: basverdi <basverdi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bastienverdier-vaissiere <bastienverdie    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 21:00:56 by basverdi          #+#    #+#             */
-/*   Updated: 2023/11/21 21:19:13 by basverdi         ###   ########.fr       */
+/*   Updated: 2023/11/22 11:06:58 by bastienverd      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-void	ft_putstr(const char *s)
+void	ft_putchar(char c)
 {
-	int i;
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *s)
+{
+	int	i;
 
 	i = 0;
 	while (s[i])
+		ft_putchar(s[i++]);
+}
+
+void	ft_putnbr(int n)
+{
+	// printf("n = %d\n", n);
+	if (n < 0)
 	{
-		write(1, &s[i], 1);
-		i++;
+		ft_putchar('-');
+		n = -n;
 	}
+	if ((n / 10) > 0)
+		ft_putnbr(n / 10);
+	ft_putchar((n % 10) + '0');
 }
 
 void	ft_check_flags(const char *s, int i, va_list ap)
 {
-	(void)s;
-	(void)i;
-	(void)ap;
+	if (s[i] == '%')
+	{
+		i++;
+		if (s[i] == 'c')
+			ft_putchar(va_arg(ap, int));
+		else if (s[i] == 's')
+			ft_putstr(va_arg(ap, char *));
+		else if (s[i] == 'p')
+			ft_putnbr(va_arg(ap, int));
+		else if (s[i] == 'd' || s[i] == 'i')
+			ft_putnbr(va_arg(ap, int));
+		else if (s[i] == '%')
+			ft_putchar('%');
+		else
+			ft_putchar(s[i]);
+	}
 }
 
 int	ft_printf(const char *s, ...)
 {
 	va_list	ap;
 	int		i;
+	int		j;
 
 	i = 0;
+	j = 0;
 	va_start(ap, s);
 	while (s[i])
 	{
 		if (s[i] == '%')
+		{
 			ft_check_flags(s, i, ap);
+			i++;
+		}
 		else
-			ft_putstr(s);
+			ft_putchar(s[i]);
 		i++;
+		j++;
 	}
 	va_end(ap);
-	return 0;
+	return (j);
 }
 
-int main()
+int	main()
 {
-	ft_printf("Hello World!");
+	int	i;
+
+	i = 0;
+	i = ft_printf("Hello %d %s !\n", 2, "World");
+	printf("i = %d\n", i);
 	return 0;
 }
