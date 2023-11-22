33c47efefc238aa40025df244ec118d529792952
Bastien Verdier-Vaissiere <basverdi@3A6.42angouleme.fr>
2023-11-22 12:54:02 +0100

    test

--- Tronc commun/Round 2/ft_printf/ft_printf.c
+++ Tronc commun/Round 2/ft_printf/ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bastienverdier-vaissiere <bastienverdie    +#+  +:+       +#+        */
+/*   By: basverdi <basverdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 21:00:56 by basverdi          #+#    #+#             */
-/*   Updated: 2023/11/22 11:06:58 by bastienverd      ###   ########.fr       */
+/*   Updated: 2023/11/22 12:53:29 by basverdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,39 +17,52 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_putstr(char *s)
+int	ft_putstr(char *s)
 {
 	int	i;
 
 	i = 0;
 	while (s[i])
 		ft_putchar(s[i++]);
+	return (i);
 }
 
-void	ft_putnbr(int n)
+int	ft_putnbr(int n)
 {
-	// printf("n = %d\n", n);
+	if (n == -2147483648)
+	{
+		ft_putstr("-2147483648");
+		return (11);
+	}
 	if (n < 0)
 	{
+		n *= -1;
 		ft_putchar('-');
-		n = -n;
 	}
-	if ((n / 10) > 0)
+	if (n < 10)
+		ft_putchar(n + 48);
+	else
+	{
 		ft_putnbr(n / 10);
-	ft_putchar((n % 10) + '0');
+		ft_putchar(n % 10 + 48);
+	}
+	return (0);
 }
 
-void	ft_check_flags(const char *s, int i, va_list ap)
+static int	ft_check_flags(const char *s, int i, va_list ap)
 {
+	int	j;
+
+	j = 0;
 	if (s[i] == '%')
 	{
 		i++;
 		if (s[i] == 'c')
 			ft_putchar(va_arg(ap, int));
 		else if (s[i] == 's')
-			ft_putstr(va_arg(ap, char *));
+			j += ft_putstr(va_arg(ap, char *)) - 1;
 		else if (s[i] == 'p')
-			ft_putnbr(va_arg(ap, int));
+			j += ft_putnbr(va_arg(ap, int)) - 1;
 		else if (s[i] == 'd' || s[i] == 'i')
 			ft_putnbr(va_arg(ap, int));
 		else if (s[i] == '%')
@@ -57,6 +70,8 @@ void	ft_check_flags(const char *s, int i, va_list ap)
 		else
 			ft_putchar(s[i]);
 	}
+	printf("check = %d\n", j);
+	return (j);
 }
 
 int	ft_printf(const char *s, ...)
@@ -68,11 +83,11 @@ int	ft_printf(const char *s, ...)
 	i = 0;
 	j = 0;
 	va_start(ap, s);
-	while (s[i])
+	while (s[i] != '\0')
 	{
 		if (s[i] == '%')
 		{
-			ft_check_flags(s, i, ap);
+			j += ft_check_flags(s, i, ap);
 			i++;
 		}
 		else
@@ -89,7 +104,7 @@ int	main()
 	int	i;
 
 	i = 0;
-	i = ft_printf("Hello %d %s !\n", 2, "World");
+	i = ft_printf("%d\n", 123);
 	printf("i = %d\n", i);
 	return 0;
 }
