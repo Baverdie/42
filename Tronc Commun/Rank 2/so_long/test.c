bd507a223b060bab3f66721c3a3b6e1841c007a2
Bastien Verdier-Vaissiere <basverdi@3F1.42angouleme.fr>
2023-12-15 20:05:06 +0100

    so_long

---
+++ Tronc Commun/Rank 2/so_long/test.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "MLX42/include/MLX42/MLX42.h"
+
+#define WIDTH 1920
+#define HEIGHT 1080
+
+static mlx_image_t* image;
+
+int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+{
+	return (r << 24 | g << 16 | b << 8 | a);
+}
+
+void ft_randomize(void* param)
+{
+	for (int32_t i = 0; i < image->width; ++i)
+	{
+		for (int32_t y = 0; y < image->height; ++y)
+		{
+			uint32_t color = ft_pixel(
+				rand() % 0xFF, // R
+				rand() % 0xFF, // G
+				rand() % 0xFF, // B
+				rand() % 0xFF  // A
+			);
+			mlx_put_pixel(image, i, y, color);
+		}
+	}
+}
+
+void ft_hook(void* param)
+{
+	mlx_t* mlx = param;
+	int y;
+	int	x;
+
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_W) && image->instances[0].y > 0)
+		image->instances[0].y -= HEIGHT / 100;
+	if (mlx_is_key_down(mlx, MLX_KEY_S) && image->instances[0].y < 960)
+		image->instances[0].y += HEIGHT / 100;
+	if (mlx_is_key_down(mlx, MLX_KEY_A) && image->instances[0].x > 0)
+		image->instances[0].x -= WIDTH / 100;
+	if (mlx_is_key_down(mlx, MLX_KEY_D) && image->instances[0].x < 1870)
+		image->instances[0].x += WIDTH / 100;
+	image->instances[0].y *= y *
+}
+
+int32_t main(int32_t argc, const char* argv[])
+{
+	mlx_t* mlx;
+
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
+	{
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	if (!(image = mlx_new_image(mlx, 50, 50)))
+	{
+		mlx_close_window(mlx);
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
+	{
+		mlx_close_window(mlx);
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	
+	mlx_loop_hook(mlx, ft_randomize, mlx);
+	mlx_loop_hook(mlx, ft_hook, mlx);
+
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
+}
