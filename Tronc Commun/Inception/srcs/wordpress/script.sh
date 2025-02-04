95e7f5bebc37d2bf38e43db94ee9d78e908cca26
basverdi <basverdi@Inception.myguest.virtualbox.org>
2025-02-04 20:34:02 +0100

    fin inception

--- Tronc Commun/Inception/srcs/wordpress/script.sh
+++ Tronc Commun/Inception/srcs/wordpress/script.sh
@@ -5,5 +5,5 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=thomarna.42.fr --title=inception --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=admin@admin.com --allow-root
+./wp-cli.phar core install --url=basverdi.42.fr --title=inception --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=admin@admin.com --allow-root
 exec "$@"
